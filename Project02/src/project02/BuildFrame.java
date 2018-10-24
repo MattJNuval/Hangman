29407378e223d0a6746f9e4b6fdf7ac5f05b0843
Mathew Nuval <nuval.mathewj@gmail.com>
2018-10-24 10:10:46 -0700

    Added Project folder

---
+++ Project02/src/project02/BuildFrame.java
@@ -0,0 +1,43 @@
+package project02;
+
+import javax.swing.*;
+
+public class BuildFrame extends JFrame {
+
+    public BuildFrame() {
+
+    }
+
+    private void buildTextField() {
+
+    }
+
+    private void buildMenu() {
+
+    }
+
+    private void buildButton() {
+
+    }
+
+    private void buildPanel() {
+
+    }
+
+    private void createFrame() {
+
+    }
+
+    private void addButtonListener() {
+
+    }
+
+    private void addMenuListener() {
+
+    }
+
+    private void addKeyListener() {
+
+    }
+
+}
